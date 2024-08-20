dcef2555dabe72abe1c6c9538763560dc79ecf36
KeyzarRasya <keyzar.r.athallah@gmail.com>
2024-08-20 11:23:38 +0700

    enabling and deactive all cors

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ mongoose.connect(DATABASE_URL)
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.use(express.static('upload'));
 
 app.use(express.urlencoded({extended:true}));
